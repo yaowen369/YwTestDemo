cac0ea7671e8c2430987271acede909bd10b72b4
yaowen <yaowen369@gmail.com>
2024-04-12 09:12:52 +0800

    初始化项目

---
+++ JavaMain/src/main/java/com/example/javamain0404/JavaMainClass.java
@@ -0,0 +1,10 @@
+package com.example.javamain0404;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class JavaMainClass {
+    public static void main(String[] args) {
+        System.out.println("11111");
+    }
+}

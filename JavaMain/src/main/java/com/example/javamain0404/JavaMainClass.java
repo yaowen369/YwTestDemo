1f8ed31cc6eaa679a36aa03f3650db130304a735
yaowen <yaowen369@gmail.com>
2024-05-23 10:19:22 +0800

    添加java main方法的Scanner的测试, 但是始终没找到如何在终端进行输入

--- JavaMain/src/main/java/com/example/javamain0404/JavaMainClass.java
+++ JavaMain/src/main/java/com/example/javamain0404/JavaMainClass.java
@@ -1,10 +1,32 @@
 package com.example.javamain0404;
 
+import java.util.Scanner;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class JavaMainClass {
     public static void main(String[] args) {
         System.out.println("11111");
+
+        for (String str : args) {
+            System.out.println(str);
+        }
+
+        System.out.println("22222");
+//
+//        Scanner scanner = new Scanner(System.in);
+//        while (scanner.hasNext()) {
+//            System.out.println("next输出:" + scanner.next());
+//        }
+//
+//        System.out.println("33333");
+//
+//        while (scanner.hasNextInt()) {
+//            System.out.println("Int输出:" + scanner.nextInt());
+//        }
+//
+//        while (scanner.hasNextInt()) {
+//            System.out.println("Line输出:" + scanner.nextLine());
+//        }
     }
 }

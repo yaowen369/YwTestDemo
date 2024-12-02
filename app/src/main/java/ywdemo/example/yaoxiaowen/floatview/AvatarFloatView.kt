efa6510053f1a808c7effc1dce4c900848b0ef10
yaowen <yaowen@bwton.com>
2024-12-02 13:50:26 +0800

    (悬浮球测试) 从网上copy了一个可拖拽和吸边的悬浮球 https://juejin.cn/post/7126475397645991972

---
+++ app/src/main/java/ywdemo/example/yaoxiaowen/floatview/AvatarFloatView.kt
@@ -0,0 +1,44 @@
+package ywdemo.example.yaoxiaowen.floatview
+
+import android.content.Context
+import android.view.View
+import android.view.ViewTreeObserver
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.shape.ShapeAppearanceModel
+import ywdemo.example.yaoxiaowen.R
+
+/**
+ * GitHub : https://github.com/yechaoa
+ * CSDN : http://blog.csdn.net/yechaoa
+ *
+ * Created by yechao on 2022/7/31.
+ * Describe :
+ */
+class AvatarFloatView(context: Context) : BaseFloatView(context) {
+
+
+    override fun getChildView(): View {
+        val imageView = ShapeableImageView(context)
+        imageView.setImageResource(R.mipmap.ic_avatar)
+        imageView.viewTreeObserver.addOnGlobalLayoutListener(object :
+            ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                imageView.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                // 获取宽度设置圆角
+                val radius = imageView.width / 2f
+                imageView.shapeAppearanceModel =
+                    ShapeAppearanceModel().toBuilder().setAllCornerSizes(radius).build()
+            }
+        })
+        return imageView
+    }
+
+    override fun getIsCanDrag(): Boolean {
+        return true
+    }
+
+
+    override fun getAdsorbTime(): Long {
+        return 3000
+    }
+}
--- app/src/main/java/ywdemo/example/yaoxiaowen/floatview/Fragment_2.kt
+++ app/src/main/java/ywdemo/example/yaoxiaowen/floatview/Fragment_2.kt
@@ -8,6 +8,8 @@ import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import ywdemo.example.yaoxiaowen.R
+import ywdemo.example.yaoxiaowen.floatview.BaseFloatView.OnFloatClickListener
+import ywdemo.example.yaoxiaowen.until.LogUtil
 
 
 class Fragment_2 : Fragment() {
@@ -32,38 +34,16 @@ class Fragment_2 : Fragment() {
         return root
     }
 
-    fun createFloatView(): FloatingBallView {
-
+    fun createFloatView(): AvatarFloatView {
         // 创建悬浮球视图
-        val floatingBallView = FloatingBallView(activity)
-        floatingBallView.setOnClickListener { // 处理点击事件
-            println("悬浮球被点击了")
-        }
+        val floatingBallView = AvatarFloatView(requireContext())
+        floatingBallView.setDragDistance(0.3)
 
-
-        // 设置悬浮球的布局参数
-//        val layoutParams = FrameLayout.LayoutParams(
-//            FrameLayout.LayoutParams.WRAP_CONTENT,
-//            FrameLayout.LayoutParams.WRAP_CONTENT
-//        )
-//        layoutParams.gravity = Gravity.LEFT or Gravity.TOP
-
-        // 获取根布局（假设是FrameLayout，根据实际情况修改）
-
-
-        floatingBallView.setOnTouchListener { v, event ->
-            val screenWidth = resources.displayMetrics.widthPixels
-            val params = floatingBallView.layoutParams as FrameLayout.LayoutParams
-            if (floatingBallView.x < screenWidth / 2) {
-                // 靠近左边
-                params.gravity = Gravity.LEFT or Gravity.TOP
-            } else {
-                // 靠近右边
-                params.gravity = Gravity.RIGHT or Gravity.TOP
+        floatingBallView.setOnFloatClickListener(object : OnFloatClickListener {
+            override fun onClick(view: View) {
+                LogUtil.i("悬浮球被点击")
             }
-            floatingBallView.layoutParams = params
-            false
-        }
+        })
 
         return floatingBallView
     }

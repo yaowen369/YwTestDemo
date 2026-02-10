d1603b2c0775a4572b304b19a370e4696ec86845
yaowen <yaowen@bwton.com>
2026-02-10 11:25:02 +0800

    添加百度人脸启动页

---
+++ app/src/main/java/ywdemo/example/yaoxiaowen/baiduface/BdStartActivity.kt
@@ -0,0 +1,13 @@
+package ywdemo.example.yaoxiaowen.baiduface
+
+import android.app.Activity
+import android.os.Bundle
+import ywdemo.example.yaoxiaowen.R
+
+class BdStartActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_bd_start)
+    }
+}

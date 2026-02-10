40f65e8971bfd186b9def481d2447c20c20a6d46
yaowen <yaowen@bwton.com>
2026-02-10 11:17:40 +0800

    把 FaceSDKAndroid/facelibrary/libs/  目录下的几个aar依赖都添加到该项目中 , 并确保编译通过

---
+++ app/src/main/java/ywdemo/example/yaoxiaowen/baiduface/BdFaceActivity.kt
@@ -0,0 +1,12 @@
+package ywdemo.example.yaoxiaowen.baiduface
+
+import android.app.Activity
+import android.os.Bundle
+import ywdemo.example.yaoxiaowen.R
+
+class BdFaceActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bd_face)
+    }
+}
